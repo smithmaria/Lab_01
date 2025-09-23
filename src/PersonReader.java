9b2e6c6b6db0bd6e6b9eab6971782de694e6b550
Maria Smith <maria.t.smith023@gmail.com>
2025-09-22 23:45:23 -0400

    edit person reader to use person class

--- src/PersonReader.java
+++ src/PersonReader.java
@@ -10,21 +10,18 @@ public class PersonReader
 {
     public static void main(String[] args)
     {
-
         JFileChooser chooser = new JFileChooser();
         File selectedFile;
         String rec = "";
-        ArrayList<String> lines = new ArrayList<>();
+        ArrayList<Person> people = new ArrayList<>();
 
         /*
-
         Here is the data file we are reading:
         000001, Bilbo, Baggins, Esq., 1060
         000002, Frodo, Baggins, Esq., 1120
         000003, Samwise, Gamgee, Esq., 1125
         000004, Peregrin, Took, Esq., 1126
         000005, Meridoc, Brandybuck, Esq., 1126
-
         */
 
         final int FIELDS_LENGTH = 5;
@@ -36,7 +33,6 @@ public class PersonReader
 
         try
         {
-
             // use the toolkit to get the current working directory of the IDE
             // Not sure if the toolkit is thread safe...
             File workingDirectory = new File(System.getProperty("user.dir"));
@@ -60,26 +56,9 @@ public class PersonReader
                 while(reader.ready())
                 {
                     rec = reader.readLine();
-                    lines.add(rec);  // read all the lines into memory in an array list
-                    line++;
-                }
-                reader.close(); // must close the file to seal it and flush buffer
-                System.out.println("\n\nData file read!");
 
-                // Now process the lines in the arrayList
-                // Split the line into the fields by using split with a comma
-                // use trim to remove leading and trailing spaces
-                // Numbers need to be converted back to numberic values. Here only
-                // the last field year of birth yob is an int the rest are strings.
-
-                String[] fields;
-
-                System.out.printf("\n%-8s%-25s%-25s%-6s%6s", "ID#", "First Name", "Last Name", "Title", "YOB");
-                System.out.print("\n======================================================================");
-
-                for(String l:lines)
-                {
-                    fields = l.split(","); // Split the record into the fields
+                    // Split the line into fields and create Person object
+                    String[] fields = rec.split(",");
 
                     if(fields.length == FIELDS_LENGTH)
                     {
@@ -88,17 +67,37 @@ public class PersonReader
                         lastName  = fields[2].trim();
                         title     = fields[3].trim();
                         yob       = Integer.parseInt(fields[4].trim());
-                        System.out.printf("\n%-8s%-25s%-25s%-6s%6d", id, firstName, lastName, title, yob);
+
+                        // Create a Person object and add to ArrayList
+                        // Note: Using your constructor order: ID, firstName, lastName, title, YOB
+                        Person person = new Person(id, firstName, lastName, title, yob);
+                        people.add(person);
                     }
                     else
                     {
                         System.out.println("Found a record that may be corrupt: ");
-                        System.out.println(l);
+                        System.out.println(rec);
                     }
+                    line++;
                 }
+                reader.close(); // must close the file to seal it and flush buffer
+                System.out.println("\n\nData file read!");
 
+                // Now display the people from the ArrayList using Person objects
+                System.out.printf("\n%-8s%-25s%-25s%-6s%6s", "ID#", "First Name", "Last Name", "Title", "YOB");
+                System.out.print("\n======================================================================");
+
+                for(Person person : people)
+                {
+                    System.out.printf("\n%-8s%-25s%-25s%-6s%6d",
+                            person.getID(),
+                            person.getFirstName(),
+                            person.getLastName(),
+                            person.getTitle(),
+                            person.getYOB());
+                }
             }
-            else  // user closed the file dialog wihtout choosing
+            else  // user closed the file dialog without choosing
             {
                 System.out.println("Failed to choose a file to process");
                 System.out.println("Run the program again!");
@@ -114,7 +113,5 @@ public class PersonReader
         {
             e.printStackTrace();
         }
-
     }
-
 }
